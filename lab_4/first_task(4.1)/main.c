dae07a6465b640771dbba4f7fdf9ca6d7f842cc5
murphy50 <80280986+murphy50@users.noreply.github.com>
2021-06-06 14:18:13 +0300

    fixes

--- lab_4/first_task(4.1)/main.c
+++ lab_4/first_task(4.1)/main.c
@@ -8,13 +8,19 @@ int main()
 {
   int n;
   printf("enter n:\n");
-  scanf("%d", &n);
+  if(!scanf("%d", &n)){
+    printf("error");
+    return 1;
+  }
   getchar();
   char* arr = (char*)malloc(n * sizeof(char));
   printf("enter string:\n");
   for (int i = 0; i < n; i++)
   {
-    scanf("%c", &arr[i]);
+    if(!scanf("%c", &arr[i])){
+      printf("error");
+      return 1;
+    }
   }
   arr[n] = '\0';
   if (IsPeriodic(arr))
