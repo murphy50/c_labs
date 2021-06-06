41b6bd8fa8aad1c54dadedd76e1e8d1ebdab2a36
murphy50 <80280986+murphy50@users.noreply.github.com>
2021-06-06 17:21:37 +0300

    fixes

--- lab_4/first_task(4.1)/IsPeriodic.h
+++ lab_4/first_task(4.1)/IsPeriodic.h
@@ -1,18 +1,19 @@
 #ifndef ISPERIODIC_H
 #define ISPERIODIC_H
-int IsPeriodic(const char a[]) {
-
+int IsPeriodic(const char a[])
+{
   int size = strlen(a);
   int flag = 1;
-  for (int j = size / 2; j>0; j--) {// цикл меняющий размер периода 
+  for (int j = size / 2; j > 0; j--)
+  { // цикл меняющий размер периода
     flag = 1;
-    for (int i = 0; i+j<size ; i++) {
-      if (a[j + i] != a[i]) {
+    for (int i = 0; i + j < size; i++){
+      if (a[j + i] != a[i]){
         flag = 0;
         break;
       }
     }
-    if (flag) {
+    if (flag){
       break;
     }
   }
